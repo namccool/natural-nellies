900def5f67e1f29ecc84bbcd247e9e6f98faff48
Nellie Ann McCool <namccool@gmail.com>
2014-04-15 00:56:09 -0700

    added interactive buttons

--- fire-cider.js
+++ fire-cider.js
@@ -5,5 +5,14 @@ $(document).ready(function(){
     $(".logo").on("mouseleave", function(){
         $(this).attr("src", "natural-nellies-black.png");
     });
-
+    $(".item").on("mouseenter", function(){
+    	$(".item").append("<i class='fa fa-heart-o' id='favorite'></i></li>");
+    	$("#favorite").on("click", function(){
+    		$(this).removeClass("fa fa-heart-o");
+    		$(this).addClass("fa fa-heart");
+    	});
+    });
+	$(".item").on("mouseleave", function(){
+		$("#favorite").remove();
+    });
 });
